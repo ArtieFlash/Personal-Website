ea2e816aca942f6ac18543572ac402eb9b904fae
Art Flachsenhar <artflachsenhar@gmail.com>
2022-06-16 12:24:22 -0700

    Added resume and organized files

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,8 @@
+// Function to change life section tabs on the About Me page
 function openTab(buttonName) {
     var i;
+    let button = "Button";
+    let activeButton = "activeButton";
     var x = document.getElementsByClassName("lifeSection");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none"
@@ -14,11 +17,17 @@ function openTab(buttonName) {
         document.getElementById(buttonName).style.display = "grid";
     }
     
-    x = document.getElementsByClassName("lifeSectionButton");
+    x = document.getElementsByClassName("lifeSection"+button);
     for (i = 0; i < x.length; i++) {
-        if (x[i].classList.contains('activeButton')) {
-            x[i].classList.remove('activeButton');
+        if (x[i].classList.contains(activeButton)) {
+            x[i].classList.remove(activeButton);
         }
     }
-    document.getElementById(buttonName+"Button").classList.add("activeButton");
+    document.getElementById(buttonName+button).classList.add(activeButton);
 }
+
+// Function to resize the width of the resume iframe wrapper
+// function resizeIframeWrapper (object) {
+//     var wrapper = document.getElementById('iframeWrapper');
+//     wrapper.style.width = object.contentWindow.document.body.scrollwidth+"px";
+// }
